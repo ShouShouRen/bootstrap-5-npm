eb9636034e68d2c2d18af1662f8dd2b90c81503a
Peter Chen <peterchen2017125@gmail.com>
2022-07-30 13:48:13 +0800

    20220730

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './index.scss'
+import 'bootstrap/js/dist/collapse'
+import Alert from 'bootstrap/js/dist/alert'
+
+function alertHandler() {
+    const alertCustomDiv = document.getElementById('alertCustom')
+    document.getElementById('alertCustomBtn').addEventListener('click', function() {
+        const alertCustom = new Alert(alertCustomDiv)
+        alertCustom.close();
+    })
+    alertCustomDiv.addEventListener('closed.bs.alert', function() {
+        console.log('closed...')
+    })
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    const alertTigger = document.getElementById('liveAlertBtn');
+
+    function alert(message, type) {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+
+        alertPlaceholder.append(wrapper)
+    }
+
+    if (alertTigger) {
+        alertTigger.addEventListener("click", function () {
+            alert('Nice, you triggered this alert message!', 'success')
+        })
+    }
+}
+
+alertHandler()
